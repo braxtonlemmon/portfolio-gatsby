39ed0101be3cc1bb39e9bce8eb6c172fb8e2c876
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-12-05 07:27:03 -0700

    Add animation to Hero.

--- src/components/BigName.jsx
+++ src/components/BigName.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+    font-size: 0em;
+  }
+  50% {
+    opacity: 0;
+    font-size: 6em;
+  }
+  100% {
+    opacity: 1;
+    font-size: 6em;
+  }
+`;
 
 const Wrapper = styled.div`
   color: white;
   h1 {
-    font-size: 6em;
+    font-size: 0;
     text-align: right;
+    animation: ${fadeIn} 4s cubic-bezier(.23,.09,.52,1.36) 2s forwards;
   }
 `;
 
