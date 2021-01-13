e451c16ca91e815907322789f1921699a4c48814
Braxton Lemmon <braxtonlemmon@gmail.com>
2021-01-13 12:14:07 -0700

    Update Bio text.

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -17,7 +17,7 @@ const Bio = () => {
     <BioBox>
       <p>
         My name is Braxton Lemmon. 
-        I received a Bachelor of Science in Recreation Management in 2014. After graduating I spent time working as a recreational therapist and learned what it means to work and communicate with teams of other professionals. As a therapist I helped people with a variety of disabilities; today I continue that effort by building websites that are accessible to everyone.
+        I received a Bachelor of Science in Recreation Management in 2014. After graduating I spent time working as a recreational therapist and learned what it means to work and communicate with a team of professionals. As a therapist, I helped people with a variety of disabilities; today I continue that effort by building websites that are accessible to everyone.
       </p>
       <p>  
         In 2018 I became a student of The Odin Project Full Stack curriculum and quickly discovered a world that encourages an ideal blend of problem solving and creativity. I've been coding ever since.
