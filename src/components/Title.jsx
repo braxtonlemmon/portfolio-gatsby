1acefe110d571430cb0bd975f640225c62f08a03
Braxton Lemmon <braxtonlemmon@gmail.com>
2021-01-14 16:31:32 -0700

    Modify responsiveness of Title.

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -10,19 +10,31 @@ const Wrapper = styled.div`
   width: 100%;
   background: ${props => props.theme.colors.lightergrey};
   color: ${props => props.theme.colors.ochre};
-  padding: 60px 0;
+  padding: 30px 0;
+  @media (min-width: 400px) {
+    padding: 40px 0;
+  }
   @media (min-width: 768px) {
+    padding: 70px 0;
+  }
+  @media (min-width: 1200px) {
     padding: 80px 0;
   }
 `;
 
 const Text = styled.h2`
-  font-size: ${props => props.theme.fontSize.four};
+  font-size: ${props => props.theme.fontSize.two};
   font-weight: bold;
   text-transform: uppercase;
   letter-spacing: 10px;
   font-family: 'Josefin Sans', sans-serif;
+  @media (min-width: 400px) {
+    font-size: ${props => props.theme.fontSize.three};
+  }
   @media (min-width: 768px) {
+    font-size: ${props => props.theme.fontSize.four};
+  }
+  @media (min-width: 1200px) {
     font-size: ${props => props.theme.fontSize.five};
   }
 `;
